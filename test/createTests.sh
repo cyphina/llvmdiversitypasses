440de488e8bec29cbe4a7b1aefae20f72d48580b
asimshrestha2 <asims178@gmail.com>
2018-04-26 14:37:16 -0700

    Adding Tests Datas

--- test/createTests.sh
+++ test/createTests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 for ((number=1;number < 4;number++))
 {
+clang++ test$number.cpp -o test$number
 clang++ -emit-llvm -c test$number.cpp -o test$number.bc
-opt -S test$number.bc > test$number.ll
+./build/bin/opt -S test$number.bc > test$number.ll
+./build/bin/opt -time-passes -load build/lib/libSkeletonPass.so -skeleton test$number.bc -o testAlloc$number.bc
+./build/bin/llc -filetype=obj testAlloc$number.bc
+gcc testAlloc$number.o -o testAlloc$number
 }
